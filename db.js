dba3a8625a7fbd94530c153568f43b501cf5e965
Steve Golden <sgolden2525@outlook.com>
2019-04-15 12:45:07 -0500

    Added connection functions
    Added a connect function that connects to the database
    
    Added an id finder to get the id
    
    Added the getDB which will get the database

--- db.js
+++ db.js
@@ -8,3 +8,28 @@ const mongoOptions = { useNewUrlParser: true }
 const state = {
   db: null
 }
+
+const connect = (cb) => {
+  if (state.db)
+    cb();
+  else {
+    mongoClient.connect(url, mongoOptions, (err, client) => {
+      if (err)
+        cb(err)
+      else {
+        state.db = client.db(dbname);
+        cb();
+      }
+    });
+  }
+}
+
+const getPrimaryKey = (_id) => {
+  return ObjectID(_id);
+}
+
+const getDB = () => {
+  return state.db;
+}
+
+module.exports = {getDB, connect, getPrimaryKey}
